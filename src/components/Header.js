18c760db02e2a49e948dbeaaf91bd36dada80310
karthi <itkarthi02@gmail.com>
2020-12-31 11:57:38 +0530

    add modified file
    add logo, button images and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import '../App.css';
+import '../App.sass';
 import { Button } from 'react-bootstrap';
 
 function Header() {
   return (
-    <div className="header">
+    <div className="header d-flex justify-content-between">
       <div className="toggle">
-        <Button type="button" className="btn btn-light">
+        <img src="logo.png" />
+        <Button type="button" className="btn btn-light ">
           Traning Mode
         </Button>
-        <Button type="button" className="btn btn-dark bg group2">
+        <Button type="button" className="btn btn-dark  ">
           Live Mode
         </Button>
+      </div>
+
+      <div className="filters">
+        <Button type="button" className="btn btn-dark  ">
+          Show All
+        </Button>
+        <Button type="button" className="btn btn-light ">
+          Crypto
+        </Button>
+        <Button type="button" className="btn btn-dark  ">
+          Commodities
+        </Button>
+        <Button type="button" className="btn btn-light ">
+          Stock
+        </Button>
+        <Button type="button" className="btn btn-dark  ">
+          Index
+        </Button>
+        <Button type="button" className="btn btn-dark  ">
+          Currency
+        </Button>
+      </div>
+
+      <div className="menu-right">
+        <img src="images/Search.png" />
+        <img src="images/Star.png" />
+
+        <img src="images/Graduating.png" />
+        <img src="images/Short.png" />
       </div>
     </div>
   );
